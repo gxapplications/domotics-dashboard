777c6435dbe442c1f9cf96462ab5c76652252e9a
Xavier Gouley <xavier.gouley@prestashop.com>
2016-10-18 16:30:44 +0200

    work on speech

--- public/js/page.js
+++ public/js/page.js
@@ -44,7 +44,7 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/* global $, angular, SpeechSynthesisUtterance */
+	/* global $, angular */
 	'use strict';
 
 	var _lodash = __webpack_require__(1);
@@ -19207,83 +19207,84 @@
 /* 8 */
 /***/ function(module, exports) {
 
-	/* global $, angular, SpeechSynthesisUtterance */
+	/* global SpeechSynthesisUtterance */
 	'use strict';
 
 	Object.defineProperty(exports, "__esModule", {
-	    value: true
+	  value: true
 	});
 
 	exports.default = function (scope) {
-	    scope.speech = {
-	        voice: undefined,
-	        lang: undefined,
-	        speak: function speak(text) {
-	            var intonation = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'normal';
+	  scope.speech = {
+	    voice: undefined,
+	    lang: undefined,
+	    speak: function speak(text) {
+	      var intonation = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'normal';
 
-	            if (!scope.speech.voice) {
-	                scope.speech.init(scope.speech.lang);
-	            }
+	      if (!scope.speech.voice) {
+	        scope.speech.init(scope.speech.lang);
+	      }
 
-	            // TODO !5: use intonation (== (normal|error))
-	            var ut = new SpeechSynthesisUtterance(text);
-	            ut.voice = scope.speech.voice;
-	            window.speechSynthesis.speak(ut);
-	        },
-	        speakLang: function speakLang(textRef) {
-	            var replacements = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-	            var intonation = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'normal';
+	      // TODO !5: use intonation (== (normal|error))
+	      var ut = new SpeechSynthesisUtterance(text);
+	      ut.voice = scope.speech.voice;
+	      window.speechSynthesis.speak(ut);
+	    },
+	    speakLang: function speakLang(textRef) {
+	      var replacements = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+	      var intonation = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'normal';
 
-	            var text = textReferences[textRef] && (textReferences[textRef][scope.speech.lang] || textReferences[textRef]['en-US']);
-	            for (var replacement in replacements) {
-	                text = text.replace('{' + replacement + '}', replacements[replacement]);
-	            }
-	            scope.speech.speak(text, intonation);
-	        },
-	        listen: function listen(grammar) {
-	            // TODO !5: commandes vocales ? ou plus tard...
-	            /*
-	             var grammar = '#JSGF V1.0; grammar colors; public <color> = rouge | bleu | rose | jaune | vert | blanc | marron | violet | mauve | noir ;'
-	             var recognition = new webkitSpeechRecognition();
-	             var speechRecognitionList = new webkitSpeechGrammarList();
-	             speechRecognitionList.addFromString(grammar, 1);
-	             recognition.grammars = speechRecognitionList;
-	             //recognition.continuous = false;
-	             recognition.lang = 'fr-FR';
-	             recognition.interimResults = false;
-	             recognition.maxAlternatives = 1;
-	             recognition.onresult = function(event) {
-	             var color = event.results[0][0].transcript;
-	             console.log("RESULT:" + color);
-	             }
-	             */
-	        },
-	        init: function init() {
-	            var lang = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'en-US';
+	      var text = textReferences[textRef] && (textReferences[textRef][scope.speech.lang] || textReferences[textRef]['en-US']);
+	      for (var replacement in replacements) {
+	        text = text.replace('{' + replacement + '}', replacements[replacement]);
+	      }
+	      scope.speech.speak(text, intonation);
+	    },
+	    listen: function listen(grammar) {
+	      // TODO !5: commandes vocales ? ou plus tard...
+	      /*
+	       var grammar = '#JSGF V1.0; grammar colors; public <color> = rouge | bleu | rose | jaune | vert | blanc | marron | violet | mauve | noir ;'
+	       var recognition = new webkitSpeechRecognition();
+	       var speechRecognitionList = new webkitSpeechGrammarList();
+	       speechRecognitionList.addFromString(grammar, 1);
+	       recognition.grammars = speechRecognitionList;
+	       //recognition.continuous = false;
+	       recognition.lang = 'fr-FR';
+	       recognition.interimResults = false;
+	       recognition.maxAlternatives = 1;
+	       recognition.onresult = function(event) {
+	       var color = event.results[0][0].transcript;
+	       console.log("RESULT:" + color);
+	       }
+	       */
+	    },
+	    init: function init() {
+	      var lang = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'en-US';
 
-	            if ('speechSynthesis' in window && window.speechSynthesis.getVoices()) {
-	                if (lang !== scope.speech.lang) {
-	                    scope.speech.voice = window.speechSynthesis.getVoices().filter(function (v) {
-	                        // available on Mint: de-DE en-US (x2! M/F) en-GB es-ES es-US fr-FR
-	                        // hi-IN id-ID it-IT ja-JP ko-KR nl-NL pl-PL pt-BR ru-RU zh-CN zh-HK zh-TW
-	                        return v.lang === lang;
-	                        // FIXME !5: on Android, what voices are available ? what is the native OS one ? (Electra :))
-	                    })[0];
-	                    scope.speech.lang = lang;
-	                }
-	            }
-	        }
-	    };
+	      scope.speech.lang = lang;
+	      if ('speechSynthesis' in window && window.speechSynthesis.getVoices()) {
+	        // FIXME !0: problem : still with german voice, even for fr-FR !
+	        scope.speech.voice = window.speechSynthesis.getVoices().filter(function (v) {
+	          // available on Mint: de-DE en-US (x2! M/F) en-GB es-ES es-US fr-FR
+	          // hi-IN id-ID it-IT ja-JP ko-KR nl-NL pl-PL pt-BR ru-RU zh-CN zh-HK zh-TW
+	          if (v.lang === lang) {
+	            console.log('#####', lang);
+	          }
+	          return v.lang === lang;
+	          // FIXME !5: on Android, what voices are available ? what is the native OS one ? (Electra :))
+	        })[0];
+	      }
+	      console.log('+++++++', lang);
+	    }
+	  };
 	};
 
 	var textReferences = {
-	    'welcome': { 'en-US': 'Welcome!', 'fr-FR': 'Bienvenue !' },
-	    'scenario_played': { 'en-US': '{title} played.', 'fr-FR': '{title} joué.' },
-	    'scenario_played_error': { 'en-US': '{title} not played properly!', 'fr-FR': '{title} n\'a pas été joué correctement !' },
-	    '': { 'en-US': '', 'fr-FR': '' }
+	  'welcome': { 'en-US': 'Welcome!', 'fr-FR': 'Bienvenue !' },
+	  'scenario_played': { 'en-US': '{title} played.', 'fr-FR': '{title} exécuté.' },
+	  'scenario_played_error': { 'en-US': '{title} not played properly!', 'fr-FR': '{title} n\'a pas été exécuté correctement !' },
+	  '': { 'en-US': '', 'fr-FR': '' }
 	};
-
-	;
 
 /***/ },
 /* 9 */
